bd49185f5229863e1b028230cc0b3bb724cf16cc
Anand Upadhya <anand.upadhya@yahoo.com>
2021-11-22 16:20:16 +0800

    Model and migration for collaborations-and-project_updates-tables

---
+++ db/migrate/20211122075304_create_collaborations.rb
@@ -0,0 +1,13 @@
+class CreateCollaborations < ActiveRecord::Migration[6.0]
+  def change
+    create_table :collaborations do |t|
+      t.references :project, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.text :details
+      t.string :status
+      t.string :role
+
+      t.timestamps
+    end
+  end
+end
