33d43c296fe3b6aef8c2a38ea38ee172676f0400
Debby Liao <debbyliaoliao@gmail.com>
2021-11-23 17:07:13 +0800

    Design_for_homepage

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the rails db:seed command (or create!d alongside the database with db:setup).
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   movies = Movie.create!([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create!(name: 'Luke', movie: movies.first)
 
 require 'faker'
 
@@ -19,20 +19,20 @@ categories = ['Graphics & Design', 'Digital Marketing', 'Writing & Translation',
               'Music & Audio', 'Programming & Tech', 'Data', 'Business', 'Lifestyle']
 
 categories.each do |category|
-  p Category.create(name: category, color_code: Faker::Color.hex_color)
+  p Category.create!(name: category, color_code: Faker::Color.hex_color)
 end
 
-# create users
+# create! users
 50.times do
   first_name = Faker::Name.first_name
   last_name = Faker::Name.last_name
   email = Faker::Internet.email
   username = Faker::Internet.username
   password = Faker::Internet.password
-  p User.create(first_name: first_name, last_name: last_name, email: email, username: username, password: password)
+  p User.create!(first_name: first_name, last_name: last_name, email: email, username: username, password: password)
 end
 
-# create projects
+# create! projects
 20.times do
   name = Faker::Lorem.sentence(word_count: 3).downcase
   description = Faker::Lorem.sentence
@@ -40,16 +40,17 @@ end
   status = "Open"
   user = User.all.sample
   category = Category.all.sample
-  p Project.create(name: name, description: description, details: details, status: status, user: user, category: category)
+
+  p Project.create!(name: name, description: description, details: details, status: status, user: user, category: category)
 end
 
-# create collaborations
+# create! collaborations
 Project.all.each do |project|
   rand(1..15).times do
     user = User.all.sample
     while user == project.user do
       user = User.all.sample
     end
-    p Collaboration.create(user: user, project: project, status: "Accepted")
+    p Collaboration.create!(user: user, project: project, status: "accepted")
   end
 end
