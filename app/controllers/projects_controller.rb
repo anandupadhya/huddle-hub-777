5cdb9772d17cdd72d2c3e78d1a197149b55e39d6
Anand Upadhya <anand.upadhya@yahoo.com>
2021-11-23 15:25:07 +0800

    Fix the new project form and controller to accomodate auto list population

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,12 +2,13 @@ class ProjectsController < ApplicationController
   before_action :set_project, only: %i[show destroy edit update] # Need to add edit and update
 
   def index
-    @projects = Project.all
+    @projects = policy_scope(Project)
     # @categories = Category.all
     if params[:category]
       p category = params[:category].to_i
       p @projects = @projects.select { |project| project.category_id == category }
     end
+
   end
 
   def show
@@ -19,14 +20,15 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new
     @categories = Category.all
+    # @statuses = ['open', 'closed', 'completed']
     authorize @project
   end
 
   def create
     # @projects = Project.all
     @project = Project.new(project_params)
-    @project.category = Category.all.sample
     @project.user = current_user
+    @project.status = 'open'
     authorize @project
 
     if @project.save
