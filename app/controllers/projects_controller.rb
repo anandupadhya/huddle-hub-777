0929fbd4f91689e342a3d0456f5d7cbac6ff05c8
Anand Upadhya <anand.upadhya@yahoo.com>
2021-11-23 13:32:26 +0800

    create projects controller and views

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,46 @@
 class ProjectsController < ApplicationController
   def index
     @projects = Project.all
+    @categories = Category.all
+    if params[:category]
+      p category = params[:category].to_i
+      p @projects = @projects.select { |project| project.category_id == category }
+    end
   end
+
+  def show
+    @project = Project.find(params[:id])
+    # @bookmark = Bookmark.new
+    # @bookmark.list = @list
+    # @movies = Movie.all
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    # @projects = Project.all
+    @project = Project.new(project_params)
+    @project.category = Category.all.sample
+    @project.user  = current_user
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      # redirect_to lists_path
+      puts "############### ERROR #{@project.errors.full_messages}"
+      render 'new'
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_path
+  end
+
+  private
+    def project_params
+      params.require(:project).permit(:name, :description, :details, :status, :user, :category)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   # root to: 'pages#home'
   root to: 'projects#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :projects
+  resources :projects do
+    resources :collaborations
+    resources :project_updates
+  end
 end
