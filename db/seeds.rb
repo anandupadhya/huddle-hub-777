d1e51f6f912220f1b64df87822198cd723a1a739
Anand Upadhya <anand.upadhya@yahoo.com>
2021-11-26 13:51:07 +0800

    Working on seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,53 +1,56 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or create!d alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create!([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create!(name: 'Luke', movie: movies.first)
+# require 'faker'
 
-require 'faker'
+# # Destroy records, DO NOT change the order
+# Profile.destroy_all
+# Collaboration.destroy_all
+# Project.destroy_all
+# Category.destroy_all
+# User.destroy_all
 
-# Destroy records, DO NOT change the order
-Profile.destroy_all
-Collaboration.destroy_all
-Project.destroy_all
-Category.destroy_all
-User.destroy_all
+# # create! users
+# count = 0
+# 50.times do
+#   first_name = Faker::Name.first_name
+#   last_name = Faker::Name.last_name
+#   email = Faker::Internet.email
+#   username = Faker::Internet.username
+#   password = "test1234"
+#   user =  User.create!(first_name: first_name, last_name: last_name, email: email, username: username, password: password)
+#   p user.profile.address = Faker::Nation.capital_city
+#   sleep 1
+#   p user.profile.save
+#   p User.find(user.id).profile
+#   count += 1
+#   if user.profile.latitude.nil?
+#     user.profile.destroy
+#     user.destroy
+#     count -= 1
+#     p "Destroyed"
+#   end
+# end
+# puts count
 
-# create! users
-count = 0
-50.times do
-  first_name = Faker::Name.first_name
-  last_name = Faker::Name.last_name
-  email = Faker::Internet.email
-  username = Faker::Internet.username
-  password = "test1234"
-  user =  User.create!(first_name: first_name, last_name: last_name, email: email, username: username, password: password)
-  p user.profile.address = Faker::Nation.capital_city
-  sleep 1
-  p user.profile.save
-  p User.find(user.id).profile
-  count += 1
-  if user.profile.latitude.nil?
-    user.profile.destroy
-    user.destroy
-    count -= 1
-    p "Destroyed"
-  end
-end
-puts count
+# categories = ['Graphics & Design', 'Digital Marketing', 'Writing & Translation', 'Video & Animation',
+#               'Music & Audio', 'Programming & Tech', 'Data', 'Business', 'Lifestyle']
 
-categories = ['Graphics & Design', 'Digital Marketing', 'Writing & Translation', 'Video & Animation',
-              'Music & Audio', 'Programming & Tech', 'Data', 'Business', 'Lifestyle']
-
-categories.each do |category|
-  Category.create!(name: category, color_code: Faker::Color.hex_color)
-end
+# categories.each do |category|
+#   Category.create!(name: category, color_code: Faker::Color.hex_color)
+# end
 
 
-# create! projects
-20.times do
+# create! projects (LOREM IPSUM)
+# 20.times do
+#   name = Faker::Lorem.sentence(word_count: 3).downcase
+#   description = Faker::Lorem.sentence
+#   details = Faker::Lorem.question(word_count: 50)
+#   status = "open"
+#   user = User.all.sample
+#   category = Category.all.sample
+#   p Project.create!(name: name, description: description, details: details, status: status, user: user, category: category)
+# end
+
+# Graphics & Design
+10.times do
   name = Faker::Lorem.sentence(word_count: 3).downcase
   description = Faker::Lorem.sentence
   details = Faker::Lorem.question(word_count: 50)
@@ -57,14 +60,18 @@ end
   p Project.create!(name: name, description: description, details: details, status: status, user: user, category: category)
 end
 
-# create! collaborations
-Project.all.each do |project|
-  rand(3..20).times do
-    user = User.all.sample
-    # check if the random user picked is not the same as the owner of the project
-    while user == project.user do
-      user = User.all.sample
-    end
-    p Collaboration.create(user: user, project: project, status: "accepted")
-  end
-end
+
+
+
+
+# # create! collaborations
+# Project.all.each do |project|
+#   rand(3..20).times do
+#     user = User.all.sample
+#     # check if the random user picked is not the same as the owner of the project
+#     while user == project.user do
+#       user = User.all.sample
+#     end
+#     p Collaboration.create(user: user, project: project, status: "accepted")
+#   end
+# end
